0875b9c47430474ffc19acfd23c8a6b71280eed6
Yarden Livnat <yarden.livnat@gmail.com>
2018-09-11 16:19:09 -0600

    move filter and reduce to the tree

--- regulus/tree/tree.py
+++ regulus/tree/tree.py
@@ -1,56 +1,8 @@
 from uuid import UUID
 from .traverse import traverse
 
-class Tree(object):
-    def __init__(self, root=None):
-        self._root = root
 
-    @property
-    def root(self):
-        return self._root
-
-    @root.setter
-    def root(self, value):
-        self._root = value
-
-    def clone(self, root=None):
-        return Tree(root)
-
-    def leaves(self, is_leaf=lambda n:n.is_leaf()):
-        if self.root is None:
-            return iter(())
-        for node in depth_first(self.root, is_leaf):
-            if is_leaf(node):
-                yield node
-
-    def items(self, **kwargs):
-        if self.root is None:
-            return iter(())
-        for node in traverse(self.root, **kwargs):
-            if node.data is not None:
-                yield node.data
-
-    def depth(self):
-        _depth = 0
-        if self.root is not None:
-            for node, d in traverse(self.root, depth=True):
-                if d > _depth:
-                    _depth = d
-        return _depth
-
-    def size(self):
-        n = 0
-        for node in self:
-            n += 1
-        return n
-
-    def __iter__(self):
-        if self._root is None:
-            return iter(())
-        return traverse(self._root)
-
-
-class Node(Tree):
+class Node(object):
     def __init__(self, id=None, data=None, parent=None, children=None, **kwargs):
         self.id = id if id is not None else data.id if data is not None else -1
         self.data = data
@@ -97,3 +49,78 @@ class Node(Tree):
         while parent:
             yield parent
             parent = parent.parent
+
+
+
+class Tree(object):
+    def __init__(self, root=None):
+        self._root = root
+
+    @property
+    def root(self):
+        return self._root
+
+    @root.setter
+    def root(self, value):
+        self._root = value
+
+    def clone(self, root=None):
+        return Tree(root)
+
+    def leaves(self, is_leaf=lambda n:n.is_leaf()):
+        if self.root is None:
+            return iter(())
+        for node in depth_first(self.root, is_leaf):
+            if is_leaf(node):
+                yield node
+
+    def items(self, **kwargs):
+        if self.root is None:
+            return iter(())
+        for node in traverse(self.root, **kwargs):
+            if node.data is not None:
+                yield node.data
+
+    def depth(self):
+        _depth = 0
+        if self.root is not None:
+            for node, d in traverse(self.root, depth=True):
+                if d > _depth:
+                    _depth = d
+        return _depth
+
+    def reduce(self, filter, factory=Node):
+        def _reduce(node, offset, depth):
+            children = []
+            child_offset = offset
+            for child in node.children:
+                children.extend(_reduce(child, child_offset, depth+1))
+                child_offset += child.data.size()
+            if filter(self, node):
+                return [factory(data=node.data, children=children, offset=offset)]
+            else:
+                return children
+
+        root = None
+        if self.root is not None:
+            root = _reduce(self.root, 0, 0)
+        return self.clone(root)
+
+    def filter(self, f):
+        select = set()
+        for node in self:
+            if f(self, node):
+                select.add(node.id)
+        return select
+
+
+    def size(self):
+        n = 0
+        for node in self:
+            n += 1
+        return n
+
+    def __iter__(self):
+        if self._root is None:
+            return iter(())
+        return traverse(self._root)
