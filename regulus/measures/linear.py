0ed4ccd304e781fc188505f73ab900a254257b90
Yarden Livnat <yarden.livnat@gmail.com>
2018-11-19 13:38:49 -0700

    remove comments

--- regulus/measures/linear.py
+++ regulus/measures/linear.py
@@ -25,7 +25,6 @@ def fitness(context, node):
     return context['linear'][node].score(node.data.x, node.data.y)
 
 
-# @cached('stepwise_fitness')
 def stepwise_fitness(context, node):
     fitnesses = []
     coefficients = np.fabs(context['linear'][node].coef_)
@@ -40,19 +39,16 @@ def stepwise_fitness(context, node):
     return fitnesses
 
 
-# @cached('relative_fitness')
 def relative_fitness(context, use_model, use_pts):
     return context['linear'][use_model].score(use_pts.data.x, use_pts.data.y)
 
 
-# @cached('parent_fiteness')
 def parent_fitness(context, node):
     if node.id == -1 or node.parent.id == -1:
         return 0
     return context['relative_fitness'][node.parent, node]
 
 
-# @cached('child_fiteness')
 def child_fitness(context, node):
     if node.id == -1 or node.parent.id == -1:
         return 0
