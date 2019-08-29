ec49ce7b8b6eaff02912c19d40feba452064fe38
Yarden Livnat <yarden.livnat@gmail.com>
2019-08-29 17:42:18 -0600

    update code

--- tests/test_reduce.py
+++ tests/test_reduce.py
@@ -1,22 +1,17 @@
 from regulus.utils import io
-from regulus.topo import *
-from regulus.alg import *
-from regulus.measures.linear import *
-from regulus.models import *
 from regulus.tree import *
 
-gauss = io.load('data/gauss4.p')
-gauss.add_attr('linear', node_model(linear_model))
-gauss.add_attr('fitness', fitness)
-gauss.add_attr('relative_fitness', relative_fitness)
-gauss.tree.add_attr('parent_fitness', parent_fitness)
-gauss.tree.add_attr('child_fitness', child_fitness)
-
+gauss = io.load('gauss4')
 
 tree = gauss.tree
 
+def f(c, n):
+    attr = c.attr['fitness']
+    value = attr[n]
+    return value > 0.4
 
-r = reduce(tree, filter=lambda n: tree.attr['fitness'][n] > 0.4 )
+
+r = tree.reduce(filter=f)
 
 print('\treduced')
 for node, depth in traverse(r.root, depth=True):
