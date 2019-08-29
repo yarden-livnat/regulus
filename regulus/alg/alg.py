6cb3d7e9e95cc009579260d67a4525f17c8508cd
Yarden Livnat <yarden.livnat@gmail.com>
2019-08-29 17:41:19 -0600

    comment out the code

--- regulus/alg/alg.py
+++ regulus/alg/alg.py
@@ -1,46 +1,37 @@
-from regulus.topo.cache import Cache
-from regulus.models import NullModel
-# from regulus.tree import reduce_tree Node
-# from regulus.topo import Partition
-
-def model_cache(model):
-    return Cache(key=lambda n: n.data.id,
-                 factory=lambda n: model(n.data) if n.data.id is not -1 else NullModel())
-
-
-def compute_model(dataset, model, cache=None):
-    if cache is None:
-        cache = model_cache(model)
-
-    for node in dataset.tree:
-        cache[node] = model(node.data)
-    return cache
-
-
-# def compute_measure(dataset, measure, models, cache=None):
+# from regulus.core.cache import Cache
+# from regulus.models import NullModel
+#
+#
+# def model_cache(model):
+#     return Cache(key=lambda n: n.data.id,
+#                  factory=lambda n: model(n.data) if n.data.id is not -1 else NullModel())
+#
+#
+# def compute_model(dataset, model, cache=None):
 #     if cache is None:
-#         cache = Cache()
+#         cache = model_cache(model)
+#
 #     for node in dataset.tree:
-#         measure(node, cache, models)
+#         cache[node] = model(node.data)
 #     return cache
-
-
-def apply_model(model_name, model, regulus):
-    regulus.attrs[model_name] = compute_model(model, regulus.tree)
-
-
-def apply_measure(measure, tree):
-    local = tree.attrs
-    context = tree.regulus.attrs
-    for node in tree:
-        measure(node, local, context)
-
-
-def compute_measure(func, tree):
-    local = tree.attrs
-    context = tree.regulus.attrs
-    cache = dict()
-    for node in tree:
-        cache[node.id] = func(node, local, context)
-    return cache
-
+#
+#
+# def apply_model(model_name, model, regulus):
+#     regulus.attrs[model_name] = compute_model(model, regulus.tree)
+#
+#
+# def apply_measure(measure, tree):
+#     local = tree.attrs
+#     context = tree.regulus.attrs
+#     for node in tree:
+#         measure(node, local, context)
+#
+#
+# def compute_measure(func, tree):
+#     local = tree.attrs
+#     context = tree.regulus.attrs
+#     cache = dict()
+#     for node in tree:
+#         cache[node.id] = func(node, local, context)
+#     return cache
+#
