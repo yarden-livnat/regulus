646590d9c0298b6fe88c34caaffba07718469719
Yarden Livnat <yarden.livnat@gmail.com>
2020-02-07 08:56:38 -0700

    initial work on proxy

---
+++ tests/bar1.py
@@ -0,0 +1,10 @@
+
+def foo(x):
+    return x+1
+
+
+f = lambda x: x+2
+
+
+def node_id(c, n):
+    return n.id+9
---
+++ tests/test-pickle1.py
@@ -0,0 +1,33 @@
+import regulus as r
+
+
+data = r.from_csv('gauss4', knn=8)
+
+
+node = data.tree.find_id(4)
+
+f = data.attr['fitness'][node]
+print('span', f)
+
+pf = data.tree.attr['parent_fitness'][node]
+print('parent fitness', pf)
+
+# data.add_attr(lambda c,n: n.id, 'node-id')
+# v = data.attr['node-id'][node]
+# print('node-id', v)
+
+
+def node_id(c,n):
+    return n.id
+
+
+data.add_attr(node_id)
+v = data.attr['node_id'][node]
+print('node-id', v)
+
+r.save(data, filename='foo')
+
+foo = r.load('foo')
+
+v = foo.attr['node_id'][node]
+print('foo node-id', v)
