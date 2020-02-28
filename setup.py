bddf2586b9fa95f0df9c15c4ee1a1fcb8f2572bf
Yarden Livnat <yarden.livnat@gmail.com>
2020-02-27 19:26:38 -0700

    topopy0.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     zip_safe=False,
     packages=find_packages(exclude=('tests', 'docs')),
     install_requires=[
-        'topopy>=0.1', 'numpy', 'sklearn', 'pandas'
+        'topopy=0.1', 'numpy', 'sklearn', 'pandas'
     ],
     test_suite='nose.collector',
     tests_require=['nose'],
